12585a229651424a21c4aab2453be9fdebdbf006
Shafeek <mohammedshafeek2112@gmail.com>
2020-12-23 18:19:24 +0800

    inital setup

---
+++ LumenAuthorsApi/app/Author.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+
+class Author extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 
+        'gender', 
+        'country'
+    ];
+
+    // /**
+    //  * The attributes excluded from the model's JSON form.
+    //  *
+    //  * @var array
+    //  */
+    // protected $hidden = [
+    //     'password',
+    // ];
+}
---
+++ LumenAuthorsApi/app/Http/Controllers/AuthorController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+
+class AuthorController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+    */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+    *   Return the list of author
+    *   @return Illuminate\Http\Response
+    */
+    public function index()
+    {
+        
+    }
+    
+    /**
+    *   Store a new author
+    *   @return Illuminate\Http\Response
+    */
+    public function store(Request $request)
+    {
+
+    }
+
+    /**
+    *   Obtain and show one author
+    *   @return Illuminate\Http\Response
+    */
+    public function show($author)
+    {
+        
+    }
+   
+    /**
+    *   Update an exiting author
+    *   @return Illuminate\Http\Response
+    */
+    public function update(Request $request, $author)
+    {
+
+    }
+
+    /**
+    *   Remove author
+    *   @return Illuminate\Http\Response
+    */
+    public function destroy($author)
+    {
+        
+    }
+}
