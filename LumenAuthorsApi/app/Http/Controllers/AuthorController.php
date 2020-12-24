90a478f00607fc901a43a323dc3c9c2222e93fb8
Shafeek <mohammedshafeek2112@gmail.com>
2020-12-24 16:52:43 +0800

    Added controller functions

--- LumenAuthorsApi/app/Http/Controllers/AuthorController.php
+++ LumenAuthorsApi/app/Http/Controllers/AuthorController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use App\Traits\ApiResponser;
+use App\Author;
 
 class AuthorController extends Controller
 {
+    use ApiResponser;
+
     /**
      * Create a new controller instance.
      *
@@ -21,7 +26,8 @@ class AuthorController extends Controller
     */
     public function index()
     {
-        
+        $authors = Author::all();
+        return $this->successResponse($authors);
     }
     
     /**
@@ -30,7 +36,16 @@ class AuthorController extends Controller
     */
     public function store(Request $request)
     {
+        $rules = [
+            'name'      => 'required|max:255',
+            'gender'    => 'required|max:255|in:male,female',
+            'country'   => 'required|max:255',
+        ];
 
+        $this->validate($request, $rules);
+
+        $author = Author::create($request->all());
+        return $this->successResponse($author);
     }
 
     /**
@@ -39,7 +54,8 @@ class AuthorController extends Controller
     */
     public function show($author)
     {
-        
+       $author = Author::findOrFail($author);
+       return $this->successResponse($author);
     }
    
     /**
@@ -48,7 +64,24 @@ class AuthorController extends Controller
     */
     public function update(Request $request, $author)
     {
+        $rules = [
+            'name'      => 'max:255',
+            'gender'    => 'max:255|in:male,female',
+            'country'   => 'max:255',
+        ];
 
+        $this->validate($request, $rules);
+
+        $author = Author::findOrFail($author);
+        $author->fill($request->all());
+
+        ## Check if anything changed in the request.
+        if($author->isClean())
+        {
+            return $this->errorResponse('Atleast one value must be changed',Response::HTTP_UNPROCESSABLE_ENTITY);
+        }
+        $author->save();
+        return $this->successResponse($author);
     }
 
     /**
@@ -57,6 +90,9 @@ class AuthorController extends Controller
     */
     public function destroy($author)
     {
-        
+        $author = Author::findOrFail($author);
+        $author->delete();
+
+        return $this->successResponse($author);
     }
 }
---
+++ LumenAuthorsApi/app/Traits/ApiResponser.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Traits;
+use Illuminate\Http\Response;
+
+trait ApiResponser {
+
+    /** 
+    *   Build Success response
+    *   @param string|array $data
+    *   @param int code.
+    *   @return illuminate\Http\JsonResponse    
+    */
+    public function successResponse($data, $code = Response::HTTP_OK)
+    {
+        return response()->json(['data' => $data], $code);
+    }
+    
+    /** 
+    *   Build error response
+    *   @param string|array $message
+    *   @param int $code.
+    *   @return illuminate\Http\JsonResponse    
+    */
+    public function errorResponse($message, $code)
+    {
+        return response()->json(['error' => $message, 'code' => $code], $code);
+    }
+}
+
+
+
+
+
+
+
+?>
